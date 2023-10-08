d4f1e1f9ab79bdccae95fd75e25a3dd619ecf865
Klasnov <2654403080@qq.com>
2023-10-08 20:43:19 +0800

    Finished version

--- id3.py
+++ id3.py
@@ -1,39 +1,50 @@
+"""
+A Python implementation of the ID3 decision tree algorithm including pruning strategy.
+Author: Yanming Shao, Baitong Lu
+Environment: Python 3.11
+"""
+
+
 import pandas as pd
 import math
 
-# 数据集读取
-train_data = pd.read_csv('./data/train.csv')
-predict_data = pd.read_csv('./data/predict.csv')
 
-# 定义一个函数计算熵
-def entropy(data, target_attr):
-    # 统计目标属性的频数
+# Define the class of decision tree nodes
+class Node:
+    def __init__(self):
+        self.attribute = None
+        self.children = {}
+        self.value = None
+
+# Compute entropy
+def entropy(data: pd.DataFrame, target_attr: str):
+    # Count the frequency of target attributes
     value_counts = data[target_attr].value_counts()
     entropy = 0
     total_records = len(data)
-    # 计算每个类别的概率并计算熵
+    # Calculate the probability of each category and calculate the entropy
     for value in value_counts:
         probability = value / total_records
         entropy += -probability * math.log2(probability)
     return entropy
 
-# 定义一个函数计算信息增益
-def information_gain(data, attribute, target_attr):
-    # 计算总体熵
+# Calculate information gain
+def information_gain(data: pd.DataFrame, attribute: str, target_attr: str):
+    # Calculate overall entropy
     total_entropy = entropy(data, target_attr)
-    # 分割数据集
+    # Split the dataset
     attribute_values = data[attribute].unique()
     weighted_entropy = 0
     for value in attribute_values:
         subset = data[data[attribute] == value]
         weight = len(subset) / len(data)
         weighted_entropy += weight * entropy(subset, target_attr)
-    # 计算信息增益
+    # Calculate information gain
     info_gain = total_entropy - weighted_entropy
     return info_gain
 
-# 定义一个函数用于选择最佳属性进行分裂
-def select_best_attribute(data, attributes, target_attr):
+# Define a function to select the best attributes for splitting
+def select_best_attribute(data: pd.DataFrame, attributes: list[str], target_attr: str):
     best_attribute = None
     max_info_gain = -1
     for attribute in attributes:
@@ -43,21 +54,14 @@ def select_best_attribute(data, attributes, target_attr):
             best_attribute = attribute
     return best_attribute
 
-# 定义决策树节点的类
-class Node:
-    def __init__(self):
-        self.attribute = None
-        self.children = {}
-        self.value = None
-
-# 构建决策树，添加最大深度剪枝
-def build_decision_tree(data, attributes, target_attr, max_depth):
-    # 如果达到最大深度或数据集只包含一个类别，则返回叶节点
+# Build a decision tree and add maximum depth pruning
+def build_decision_tree(data: pd.DataFrame, attributes: list[str], target_attr: str, max_depth: int):
+    # If the maximum depth is reached or the dataset contains only one category, return leaf nodes
     if max_depth == 0 or len(data[target_attr].unique()) == 1:
         leaf = Node()
         leaf.value = data[target_attr].iloc[0]
         return leaf
-    # 如果没有可用属性，返回叶节点，类别为数据集中出现最多的类别
+    # If there are no available attributes, return the leaf node, and the category is the category that appears most in the data set.
     if len(attributes) == 0:
         leaf = Node()
         leaf.value = data[target_attr].value_counts().idxmax()
@@ -77,30 +81,49 @@ def build_decision_tree(data, attributes, target_attr, max_depth):
             tree.children[value] = build_decision_tree(subset, new_attributes, target_attr, max_depth - 1)
     return tree
 
-# 预测函数
-def predict(tree, data):
-    if tree.value is not None:
-        return tree.value
-    attribute_value = data[tree.attribute]
-    if attribute_value in tree.children:
-        return predict(tree.children[attribute_value], data)
+# When processing unlearned queries, return the category or most categories of a decision tree node
+def most_attribute(tree_node: Node):
+    # If the node is a leaf node, return the category of the leaf node
+    if not tree_node.children:
+        return tree_node.value
+    # If the node is not a leaf node, find the category that appears most in the child node
+    child_values = [most_attribute(child) for child in tree_node.children.values()]
+    return max(child_values, key=child_values.count)
+
+# Prediction function
+def predict(tree_node: Node, data: pd.Series):
+    if tree_node.value is not None:
+        return tree_node.value
+    attribute_value = data[tree_node.attribute]
+    if attribute_value in tree_node.children:
+        return predict(tree_node.children[attribute_value], data)
     else:
-        return data['weather'].value_counts().idxmax()
+        return most_attribute(tree_node)
 
-# 构建决策树
-attributes = train_data.columns[:-1].tolist()
-target_attribute = 'weather'
+# Main function
+def main():
+    #Dataset reading
+    train_data = pd.read_csv('./data/train.csv')
+    predict_data = pd.read_csv('./data/predict.csv')
 
-# 构建决策树并应用最大深度剪枝
-max_depth = 5  # 你可以根据需要调整最大深度
-decision_tree = build_decision_tree(train_data, attributes, target_attribute, max_depth)
+    # Build decision tree
+    attributes = train_data.columns[:-1].tolist()
+    target_attribute = 'weather'
 
-# 预测并输出结果
-predicted_results = []
-for index, row in predict_data.iterrows():
-    prediction = predict(decision_tree, row)
-    predicted_results.append(prediction)
+    # Build a decision tree and apply maximum depth pruning
+    max_depth = 5
+    decision_tree = build_decision_tree(train_data, attributes, target_attribute, max_depth)
 
-# 存储预测结果
-predict_data['weather'] = predicted_results
-predict_data.to_csv('./data/result.csv', index=False)
+    # Predict and output the results
+    predicted_results = []
+    for _, row in predict_data.iterrows():
+        prediction = predict(decision_tree, row)
+        predicted_results.append(prediction)
+
+    # Store prediction results
+    predict_data['weather'] = predicted_results
+    predict_data.to_csv('./data/result.csv', index=False)
+
+
+if __name__ == '__main__':
+    main()
